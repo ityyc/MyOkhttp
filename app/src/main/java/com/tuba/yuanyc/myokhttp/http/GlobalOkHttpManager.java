849b6e5fc5ddab50a4f685724ba30f56a9a242f2
yuanyc <yuanyc@mapbar.com>
2017-06-09 10:35:41 +0800

    chg 添加okhttp相关依赖包

---
+++ app/src/main/java/com/tuba/yuanyc/myokhttp/http/GlobalOkHttpManager.java
@@ -0,0 +1,13 @@
+package com.tuba.yuanyc.myokhttp.http;
+
+/**
+ * Author ：yuanyc
+ * Time ：2017/6/9
+ * Description ：
+ */
+
+public class GlobalOkHttpManager {
+    public static void sendRequest() {
+
+    }
+}
